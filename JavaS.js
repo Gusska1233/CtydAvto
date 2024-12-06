319ead5833dd087c0a88b742df862246fa6d39b9
Gusska1233 <vitekap07@gmail.com>
2024-12-06 20:40:09 +0500

    Add files via upload

---
+++ JavaS.js
@@ -0,0 +1,7 @@
+document.getElementById('contactForm').onsubmit = function(event) {
+event.preventDefault();
+    alert("Форма отправлена!");
+};
+function info(){
+ alert("Привет, мой дорогой пользователь! Этот сайт сделал Палехов Виктор из БАС-1 1 курс!");
+}
